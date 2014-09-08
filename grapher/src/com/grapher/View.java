9746635c7dfc2da1d1cbf7bd3fec9714888fe734
17patelumang <17patelumang@gmail.com>
2014-09-08 02:47:07 -0400

    Update View.java

--- grapher/src/com/grapher/View.java
+++ grapher/src/com/grapher/View.java
@@ -20,7 +20,7 @@ import java.util.Collections;
 import java.util.HashMap;
 
 import org.eclipse.jface.window.Window;
-import org.eclipse.osgi.framework.internal.core.ConsoleManager;
+//import org.eclipse.osgi.framework.internal.core.ConsoleManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
